b85949d709c7292168325e64ae829d530a341690
Alex Betts <38247452+lina2497@users.noreply.github.com>
2020-11-02 14:57:51 +0000

    Update gdal.sh

--- gdal.sh
+++ gdal.sh
@@ -5,10 +5,6 @@ yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarc
 yum-config-manager --enable epel
 yum install gedit libXcomposite libXcursor libXi libXtst libXrandr alsa-lib mesa-libEGL libXdamage mesa-libGL libXScrnSaver
 
-#install rstudio server
-wget https://download2.rstudio.org/server/centos6/x86_64/rstudio-server-rhel-1.3.1093-x86_64.rpm
-yum install rstudio-server-rhel-1.3.1093-x86_64.rpm
-
 #install r package dependencies
 yum install udunits2-devel gdal
 yum -y install make automake gcc gcc-c++ libcurl-devel proj-devel geos-devel proj-nad proj-epsg
